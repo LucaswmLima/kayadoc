b964f5844f419ad4687a4853fc52eb1ff424f11d
Lucas William <lucaswilliamml@gmail.com>
2025-03-08 16:56:18 -0300

    fontawesome config

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -8,14 +8,14 @@ module.exports = {
     extend: {
       fontFamily: {},
       colors: {
+        theme_color1: {
+          600: "rgb(35 35 35 / var(--tw-bg-opacity, 1))",
+        },
         theme_color2: {
           400: "rgb(115 130 82 / var(--tw-text-opacity,1))",
           300: "rgb(167 200 121 / var(--tw-bg-opacity, 1))",
           200: "rgb(205 234 152 / var(--tw-bg-opacity, 1))",
           100: "rgb(224 244 179 / var(--tw-bg-opacity,1))",
-        },
-        bg_theme_color1: {
-          600: "rgb(35 35 35 / var(--tw-bg-opacity, 1))",
         },
       },
     },
