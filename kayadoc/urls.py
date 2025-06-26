15362aeb800dc6510c2a08f323f17595c76552fa
Lucas William <lucaswilliamml@gmail.com>
2025-06-26 17:24:49 -0300

    Update static and media serv

--- kayadoc/urls.py
+++ kayadoc/urls.py
@@ -3,14 +3,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('medicos/', include('medicos.urls')),
-    
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve estáticos e mídia sempre, mesmo com DEBUG=False (apenas para desenvolvimento/teste)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
